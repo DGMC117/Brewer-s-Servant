c27ff00372d18ef96a59f97f1c0c8715ce01917e
DGMC117 <DGMC117@users.noreply.github.com>
2022-02-13 19:34:08 +0100

    Adds: Table creation SQL statements.

--- app/src/main/java/magicchief/main/brewersservant/DBHelper.kt
+++ app/src/main/java/magicchief/main/brewersservant/DBHelper.kt
@@ -11,36 +11,100 @@ class DBHelper (context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
         // Create Card table
         val createCardDB = (
                 "CREATE TABLE $CARD_TABLE_NAME ("
+                        + CARD_ID + " int primary key,"
+                        + CARD_CMC + " float not null,"
+                        + CARD_COLOR_IDENTITY + " varchar(50) not null,"
+                        + CARD_COLORS + " varchar(50),"
+                        + CARD_LAYOUT + " varchar(50) not null,"
+                        + CARD_LOYALTY + " varchar(25),"
+                        + CARD_MANA_COST + " varchar(100),"
+                        + CARD_NAME + " varchar(255) not null,"
+                        + CARD_ORACLE_TEXT + " text,"
+                        + CARD_POWER + " varchar(25),"
+                        + CARD_PRODUCED_MANA + " varchar(100),"
+                        + CARD_TOUGHNESS + " varchar(25),"
+                        + CARD_TYPE_LINE + "varchar(255) not null,"
+                        + CARD_ARTIST + " varchar(50),"
+                        + CARD_FLAVOR_TEXT + " text,"
+                        + CARD_RARITY + " varchar(50) not null,"
+                        + CARD_SET_ID + " int not null foreign key references $CARD_SET_TABLE_NAME($CARD_SET_SET_ID),"
+                        + CARD_PRICE_USD + " varchar(25) not null,"
+                        + CARD_PRICE_EUR + " varchar(25) not null,"
+                        + CARD_PRICE_TIX + " varchar(25) not null,"
+                        + CARD_LEGAL_STANDARD + " varchar(25) not null,"
+                        + CARD_LEGAL_PIONEER + " varchar(25) not null,"
+                        + CARD_LEGAL_MODERN + " varchar(25) not null,"
+                        + CARD_LEGAL_LEGACY + " varchar(25) not null,"
+                        + CARD_LEGAL_VINTAGE + " varchar(25) not null,"
+                        + CARD_LEGAL_BRAWL + " varchar(25) not null,"
+                        + CARD_LEGAL_HISTORIC + " varchar(25) not null,"
+                        + CARD_LEGAL_PAUPER + " varchar(25) not null,"
+                        + CARD_LEGAL_PENNY + " varchar(25) not null,"
+                        + CARD_LEGAL_COMMANDER + " varchar(25) not null,"
+                        + CARD_IMAGE_PNG + " varchar(255),"
+                        + CARD_IMAGE_BORDER_CROP + " varchar(255),"
+                        + CARD_IMAGE_ART_CROP + " varchar(255),"
+                        + CARD_IMAGE_LARGE + " varchar(255),"
+                        + CARD_IMAGE_NORMAL + " varchar(255),"
+                        + CARD_IMAGE_SMALL + " varchar(255),"
                         + ")"
                 )
         // Create CardFace table
         val createCardFaceDB = (
                 "CREATE TABLE $CARD_FACE_TABLE_NAME ("
+                        + CARD_FACE_ID + " int auto_increment primary key,"
+                        + CARD_FACE_ID_MAIN_CARD + " int not null foreign key references $CARD_TABLE_NAME($CARD_ID),"
+                        + CARD_FACE_CMC + " float,"
+                        + CARD_FACE_COLORS + " varchar(50),"
+                        + CARD_FACE_LAYOUT + " varchar(50),"
+                        + CARD_FACE_LOYALTY + " varchar(25),"
+                        + CARD_FACE_MANA_COST + " varchar(100) not null,"
+                        + CARD_FACE_NAME + " varchar(255) not null,"
+                        + CARD_FACE_ORACLE_TEXT + " text,"
+                        + CARD_FACE_POWER + " varchar(25),"
+                        + CARD_FACE_TOUGHNESS + " varchar(25),"
+                        + CARD_FACE_TYPE_LINE + "varchar(255),"
+                        + CARD_FACE_ARTIST + " varchar(50),"
+                        + CARD_FACE_FLAVOR_TEXT + " text,"
+                        + CARD_FACE_IMAGE_PNG + " varchar(255),"
+                        + CARD_FACE_IMAGE_BORDER_CROP + " varchar(255),"
+                        + CARD_FACE_IMAGE_ART_CROP + " varchar(255),"
+                        + CARD_FACE_IMAGE_LARGE + " varchar(255),"
+                        + CARD_FACE_IMAGE_NORMAL + " varchar(255),"
+                        + CARD_FACE_IMAGE_SMALL + " varchar(255),"
                         + ")"
                 )
         // Create RelatedCard table
         val createRelatedCardDB = (
                 "CREATE TABLE $RELATED_CARD_TABLE_NAME ("
+                        + RELATED_CARD_ID_MAIN + " int foreign key references $CARD_TABLE_NAME($CARD_ID),"
+                        + RELATED_CARD_ID_RELATED + " int foreign key references $CARD_TABLE_NAME($CARD_ID),"
+                        + RELATED_CARD_COMPONENT + " varchar(25) not null,"
+                        + "primary key ($RELATED_CARD_ID_MAIN,$RELATED_CARD_ID_RELATED),"
                         + ")"
                 )
         // Create CardSet table
         val createCardSetDB = (
                 "CREATE TABLE $CARD_SET_TABLE_NAME ("
+                        + CARD_SET_SET_ID + " int primary key,"
+                        + CARD_SET_CODE + " varchar(10) not null,"
+                        + CARD_SET_NAME + " varchar(50) not null,"
+                        + CARD_SET_ICON_SVG_URI + " varchar(255) not null,"
                         + ")"
                 )
 
         // Execute SQL code
+        db.execSQL(createCardSetDB)
         db.execSQL(createCardDB)
         db.execSQL(createCardFaceDB)
         db.execSQL(createRelatedCardDB)
-        db.execSQL(createCardSetDB)
     }
 
     override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
         // This method drops the tables if they existed and creates them again
-        db.execSQL("DROP TABLE IF EXISTS $CARD_TABLE_NAME")
         db.execSQL("DROP TABLE IF EXISTS $CARD_FACE_TABLE_NAME")
         db.execSQL("DROP TABLE IF EXISTS $RELATED_CARD_TABLE_NAME")
+        db.execSQL("DROP TABLE IF EXISTS $CARD_TABLE_NAME")
         db.execSQL("DROP TABLE IF EXISTS $CARD_SET_TABLE_NAME")
 
         onCreate(db)
@@ -132,7 +196,7 @@ class DBHelper (context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null
         private val CARD_SET_SET_ID = "id"
         private val CARD_SET_CODE = "code"
         private val CARD_SET_NAME = "name"
-        private val CARD_SET_ICON_SVG_URL = "icon_svg_uri"
+        private val CARD_SET_ICON_SVG_URI = "icon_svg_uri"
 
     }
 }
