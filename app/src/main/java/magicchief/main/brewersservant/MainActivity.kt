e7ea48d1571dd2fe9a4a210e7c56a255dacdc38a
DGMC117 <DGMC117@users.noreply.github.com>
2022-03-05 17:55:43 +0100

    Adds: Intent to card download activity.

--- app/src/main/java/magicchief/main/brewersservant/MainActivity.kt
+++ app/src/main/java/magicchief/main/brewersservant/MainActivity.kt
@@ -1,5 +1,6 @@
 package magicchief.main.brewersservant
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        intent = Intent (this, CardDownloadActivity::class.java)
+        startActivity(intent)
     }
 }
