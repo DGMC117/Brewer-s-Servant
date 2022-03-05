d982aab91caf8cbc8f7b2996c42a6e4684132838
DGMC117 <DGMC117@users.noreply.github.com>
2022-03-05 18:25:40 +0100

    Adds: CardListActivity.kt activity, layout and adds entry on manifest.

---
+++ app/src/main/java/magicchief/main/brewersservant/CardListActivity.kt
@@ -0,0 +1,11 @@
+package magicchief.main.brewersservant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_list)
+    }
+}
