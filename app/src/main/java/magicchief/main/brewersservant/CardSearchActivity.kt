e7bdbaf2eb7f42340df70963a007179f5dd2d19d
DGMC117 <DGMC117@users.noreply.github.com>
2022-03-06 19:49:13 +0100

    Adds: Card Search activity framework.

---
+++ app/src/main/java/magicchief/main/brewersservant/CardSearchActivity.kt
@@ -0,0 +1,11 @@
+package magicchief.main.brewersservant
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CardSearchActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_card_search)
+    }
+}
